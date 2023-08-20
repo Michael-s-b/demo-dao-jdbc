3839082fef967c6f6e7c00f0405ad8d4d4a43f2c
michael-s-b <michelsilva098@gmail.com>
2023-08-20 15:39:16 -0300

    -implements insert method on SellerDaoJDBC -add test 4 for seller insert

--- src/application/App.java
+++ src/application/App.java
@@ -8,13 +8,21 @@ import model.entities.Seller;
 public class App {
     public static void main(String[] args) throws Exception {
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        /////////////////////////////////////////////////
         System.out.println("\n=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(5);
         System.out.println(seller);
+        /////////////////////////////////////////////////
         System.out.println("\n=== TEST 2: seller findByDepartment ===");
         Department department = new Department(2, null);
         sellerDao.findByDepartment(department).forEach(System.out::println);
+        /////////////////////////////////////////////////
         System.out.println("\n=== TEST 3: seller findAll ===");
         sellerDao.findAll().forEach(System.out::println);
+        /////////////////////////////////////////////////
+        System.out.println("\n=== TEST 4: seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new java.util.Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
